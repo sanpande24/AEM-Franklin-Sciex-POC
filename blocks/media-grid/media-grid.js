a650ae9b421b74a041a4de38cda128edcc2ee85a
sanpande24 <sandeep.pande@gspann.com>
2024-09-25 00:40:54 +0530

    Committing to fix trailing error issue in media-grid -3

--- blocks/media-grid/media-grid.js
+++ blocks/media-grid/media-grid.js
@@ -7,8 +7,8 @@ export default function decorate(block) {
   flexDiv.className = 'tw-flex tw-flex-col md:tw-flex-row md:tw-flex-wrap';
   block.classList.add('tw');
   [...block.children].forEach((row) => {
-    const newCol = document.createElement('div');
-    newCol.className ='tw-w-full md:tw-w-1/2 tw-flex tw-flex-col media-grid-item  md:tw-pt-0 false';
+    const col = document.createElement('div');
+    col.className = 'tw-w-full md:tw-w-1/2 tw-flex tw-flex-col media-grid-item md:tw-pt-0 false';
     const pic = row.querySelector ('picture');
     const imageContainer = document.createElement('div');
     const link = row.querySelector('a');
@@ -19,8 +19,8 @@ export default function decorate(block) {
       imgW.className = 'tw-transition-all tw-duration-500 tw-inset-0 tw-top-0 tw-left-0 tw-w-full tw-h-full tw-object-cover hover:tw-scale-[1.05] motion-reduce:hover:tw-transform-none';
       link.appendChild(pic);
       imageContainer.appendChild(link);
-      newCol.appendChild(imageContainer);
-      flexDiv.appendChild(newCol);
+      col.appendChild(imageContainer);
+      flexDiv.appendChild(col);
     }
   });
   container.appendChild(flexDiv);
