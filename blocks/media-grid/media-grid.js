1e3d9cb52fa1dec55c9dd927cb70c3d24faebf50
sanpande24 <sandeep.pande@gspann.com>
2024-09-25 00:26:35 +0530

    Committing to fix trailing error issue in media-grid

--- blocks/media-grid/media-grid.js
+++ blocks/media-grid/media-grid.js
@@ -1,11 +1,11 @@
-export default function decorate(block) {
-  block.classList.add('tw');  
+export default function decorate(block) { 
   const section = document.createElement('section');
   section.className = 'tw-pt-32 md:tw-pt-48 tw-pb-32 md:tw-pb-48';
   const container = document.createElement('div');
   container.className = 'tw-container';
   const flexDiv = document.createElement('div');
   flexDiv.className = 'tw-flex tw-flex-col md:tw-flex-row md:tw-flex-wrap';
+  block.classList.add('tw'); 
   [...block.children].forEach((row) => {
     const newCol = document.createElement('div');
     newCol.className ='tw-w-full md:tw-w-1/2 tw-flex tw-flex-col media-grid-item  md:tw-pt-0 false';
@@ -13,20 +13,20 @@ export default function decorate(block) {
     const imageContainer = document.createElement('div');
     const link = row.querySelector('a');
     link.target = '_blank';
-    if(row.children.length ===1){
-        imageContainer.className = 'tw-w-full tw-relative tw-overflow-hidden tw-aspect-[8/5]';
-        const imgW = pic.querySelector('img'); 
-        imgW.className = 'tw-transition-all tw-duration-500 tw-inset-0 tw-top-0 tw-left-0 tw-w-full tw-h-full tw-object-cover hover:tw-scale-[1.05] motion-reduce:hover:tw-transform-none';
-        link.appendChild(pic);
-        imageContainer.appendChild(link);
-        newCol.appendChild(imageContainer);
-        flexDiv.appendChild(newCol);
+    if (row.children.length === 1) {
+      imageContainer.className = 'tw-w-full tw-relative tw-overflow-hidden tw-aspect-[8/5]';
+      const imgW = pic.querySelector('img'); 
+      imgW.className = 'tw-transition-all tw-duration-500 tw-inset-0 tw-top-0 tw-left-0 tw-w-full tw-h-full tw-object-cover hover:tw-scale-[1.05] motion-reduce:hover:tw-transform-none';
+      link.appendChild(pic);
+      imageContainer.appendChild(link);
+      newCol.appendChild(imageContainer);
+      flexDiv.appendChild(newCol);
     }
 
   });
 
-    container.appendChild(flexDiv);
-    section.appendChild(container);
-    block.textContent = '';
-    block.appendChild(section);
+  container.appendChild(flexDiv);
+  section.appendChild(container);
+  block.textContent = '';
+  block.appendChild(section);
 }
